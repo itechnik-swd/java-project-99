a45a5ccdb736acc04800dbbe6c1ad215ac322ad7
itechnik <itechnik.swd@gmail.com>
2025-02-17 00:12:48 +0300

    Step 1 is completed

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    checkstyle
+    application
+    jacoco
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("io.freefair.lombok") version "8.12.1"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.AppApplication"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
